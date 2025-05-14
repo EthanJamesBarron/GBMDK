ffa1d6a2c22b80aca3ee3035b0ed6103f9e00eda
TheUltimateNuke <nuke.public@gmail.com>
2025-05-14 15:54:12 -0400

    Set ModName during template test

--- Scripts/Editor/Testing/TestTemplates.cs
+++ Scripts/Editor/Testing/TestTemplates.cs
@@ -1,3 +1,4 @@
+using CoreNet.Messaging.Messages;
 using UnityEditor;
 using UnityEditor.AddressableAssets;
 
@@ -5,13 +6,21 @@ namespace GBMDK.Editor
 {
     public static class TestTemplates
     {
+        public const string TemplateModName = "GBMDKTemplatesMod";
+        
         [MenuItem("GBMDK/Testing/Test GBMDK Templates")]
         public static void Test()
         {
-            ContentStarters.CreateMapStuff("Assets/Mods/GBMDKTemplatesMod/Maps");
-            ContentStarters.CreateCostumeStuff("Assets/Mods/GBMDKTemplatesMod/Costumes");
-            
+            ContentStarters.CreateMapStuff($"Assets/Mods/{TemplateModName}/Maps/NewMap");
+            ContentStarters.CreateCostumeStuff($"Assets/Mods/{TemplateModName}/Costumes/NewCostume");
+            var prevModName = AddressableAssetSettingsDefaultObject.Settings.profileSettings.GetValueByName(
+                AddressableAssetSettingsDefaultObject.Settings.activeProfileId, "ModName");
+            AddressableAssetSettingsDefaultObject.Settings.profileSettings.SetValue(
+                AddressableAssetSettingsDefaultObject.Settings.activeProfileId, "ModName", $"{TemplateModName}");
             ModTester.TestMod();
+            AddressableAssetSettingsDefaultObject.Settings.profileSettings.SetValue(
+                AddressableAssetSettingsDefaultObject.Settings.activeProfileId, "ModName", prevModName);
+            AssetDatabase.DeleteAsset($"Assets/Mods/{TemplateModName}");
         }
     }
 }
